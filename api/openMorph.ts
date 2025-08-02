be54c53f6b03232252ab1352fb9221a88004fc42
Pandalfer <149884072+Pandalfer@users.noreply.github.com>
2025-08-02 21:40:15 +0100

    Delete api directory

--- api/openMorph.ts
+++
@@ -1,7 +0,0 @@
-export function openMorph(src: any): any {
-  const dst = new cv.Mat();
-  const kernel = cv.Mat.ones(3, 3, cv.CV_8U);
-  cv.morphologyEx(src, dst, cv.MORPH_CLOSE, kernel);
-  kernel.delete();
-  return dst;
-}
